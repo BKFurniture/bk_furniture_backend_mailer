a2111f0a1efa6dc858673ce51a98671dd4f8b340
Phuoc Nguyen <killerdn2001@gmail.com>
2023-03-18 19:58:29 +0700

    fix content

---
+++ src/main/java/com/bkfunirture/mailer/service/MailerService.java
@@ -0,0 +1,45 @@
+package com.bkfunirture.mailer.service;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class MailerService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private Configuration config;
+    @Value("${spring.mail.username}") private String sender;
+    @Value("${url}") private String url;
+    public String signUpSuccess(String gmail,String userFullName) {
+        try {
+            Template template = config.getTemplate("sign-up.ftl");
+            Map<String, Object> model = new HashMap<>();
+            model.put("userFullName",userFullName);
+            model.put("url",url);
+            MimeMessage mimeMessage=javaMailSender.createMimeMessage();
+            MimeMessageHelper helper=new MimeMessageHelper(mimeMessage,"utf-8");
+            String htmlMsg= FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+            helper.setText(htmlMsg, true); // Use this or above line.
+            helper.setTo(gmail);
+            helper.setSubject("Welcome to BKFurniture");
+            helper.setFrom(sender);
+            javaMailSender.send(mimeMessage);
+            return "Mail signup success";
+        }
+        catch (Exception e){
+            return "Error mail signup";
+        }
+    }
+}
---
+++ src/main/resources/templates/sign-up.ftl
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
+</head>
+<body style="background-color: #C9ECFF;display: flex;-ms-flex-pack: center !important;justify-content: center;">
+	<div style="  position: relative;width: 100%;margin-left:30%;flex: 0 0 41.666667%;max-width: 41.666667%;background-color: #fff !important;">
+    <img style="display: block !important;width: 100% !important;" src="https://i.imgur.com/QwGdThQ.png" alt="">
+
+    <div style="padding: 1rem !important;display: block !important;">
+			<h3 style="margin-top:0">
+				Hello, <strong>${userFullName}</strong>, Welcome to BK Furniture.
+			</h3>
+      <p>
+        You have successfully signed up. To login to the site, just follow this link:
+        <a href="${url}">BK Funiture</a>
+      </p>
+      <p>
+        Thanks for joining and have a great day!
+      </p>
+    </div>
+    <div style="background-color: white">
+        <img src="https://i.imgur.com/fbdabHZ.png" alt="" style="display: block !important;width: 100% !important;">
+    </div>
+  </div>
+</body>
+</html>
